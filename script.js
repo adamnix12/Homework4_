f42ddbcb84a2ddbbbac893be25aeab2225dc0b29
adamnix12 <adamnix@iu.edu>
2026-09-22 14:03:22 -0400

    Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const num1 = Number(prompt("Enter a number"));
+
+const num2 = Number(prompt("Enter a number 2"));
+
+const num3 = Number(prompt("Enter a number 3"));
+
+console.log((num1+num2+num3)/3);
+
+const thing1 = "Fill in the blank:"
+const thing2 = " The"
+const thing3 = " test"
+
+// prompt(thing1+" is working")
+
+prompt((thing1 + thing2 + thing3)+" _____ working.")
+
+const now = new Date();
+console.log(now.toString()); 
